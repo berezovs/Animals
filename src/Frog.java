b4e98728061fd8f0ab1efa651d08192d179ae9fc
Serghei Berezovschi <sb206@students.uwf.edu>
2020-01-28 14:54:50 -0600

    first commit

---
+++ src/Frog.java
@@ -0,0 +1,13 @@
+
+public class Frog {
+	private String color;
+
+	Frog() {
+		this("");
+	}
+
+	Frog(String color) {
+		this.color = color;
+	}
+
+}
---
+++ src/FrogTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+public class FrogTest {
+
+	@Test
+	public void test() {
+		Frog t = new Frog();
+
+		Frog redFrog = new Frog("red");
+	}
+
+}
